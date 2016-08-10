28518aac8c735917512c9948a75b714b04ab4f71
being-developer <shubhxx54@gmail.com>
2016-08-10 14:55:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
-<script>var pathvars = {worker: '/service-worker.js',manifest: '/manifest.json'};var quvu = {};quvu.websiteId='57a9bb9581556b791979cbfd';quvu.segmentName  = 'hello';quvu.serverUrl = 'https://www.quvu.com/api/';</script><script type='text/javascript' src='https://cdn.quvu.com/launchpad.js'></script>
+<script>var pathvars = {worker: '/service-worker.js',manifest: '/manifest.json'};var quvu = {};quvu.websiteId='57a9bb9581556b791979cbfd';quvu.segmentName  = 'default';quvu.serverUrl = 'https://www.quvu.com/api/';</script><script type='text/javascript' src='https://cdn.quvu.com/launchpad.js'></script>
 </head>
 <style>
 
