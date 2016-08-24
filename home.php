1ab45d21cf4efd9a93ea08186a6eadafa7133f7b
being-developer <shubhxx54@gmail.com>
2016-08-24 21:27:57 +0530

    fafdsaf

--- home.php
+++ home.php
@@ -1,4 +1,17 @@
 <head>
+    <meta charset="utf-8">
+    <title>API Documentation </title>
+    <link rel="stylesheet" href="jquery-ui.css">
+    <script src="jquery.js"></script>
+    <script src="jquery-ui.js"></script>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
 <script>var pathvars = {worker: '/service-worker.js',manifest: '/manifest.json'};var quvu = {};quvu.websiteId='57a9bb9581556b791979cbfd';quvu.segmentName  = 'hello';quvu.serverUrl = 'https://www.quvu.com/api/';</script><script type='text/javascript' src='https://cdn.quvu.com/launchpad.js'></script>
     </head><body>
