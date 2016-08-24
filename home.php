3bce89184105ff55ee2a441d77fac8c71eec82c6
being-developer <shubhxx54@gmail.com>
2016-08-24 21:24:29 +0530

    dsf

--- home.php
+++ home.php
@@ -1,3 +1,6 @@
-<body>
+<head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
+<script>var pathvars = {worker: '/service-worker.js',manifest: '/manifest.json'};var quvu = {};quvu.websiteId='57a9bb9581556b791979cbfd';quvu.segmentName  = 'hello';quvu.serverUrl = 'https://www.quvu.com/api/';</script><script type='text/javascript' src='https://cdn.quvu.com/launchpad.js'></script>
+    </head><body>
     <h3>New Pages</h3>
     </body>
